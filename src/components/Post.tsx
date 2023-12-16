ecfc45cff4c47dfe420428c4c95391341e8a4af2
dv0c <tasosmeidanis12@gmail.com>
2023-12-16 11:46:00 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/(routes)/(home)/_components/whats-new.tsx 	modified:   src/components/Post.tsx 	modified:   src/lib/utils.ts

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,69 +1,127 @@
 import { FrauncesFont } from "@/lib/fonts";
-import { cn, formatTimeToNow } from "@/lib/utils";
+import getBase64, { cn, formatTimeToNow } from "@/lib/utils";
 import { Post, Tag } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
-import { FC } from "react";
 
 interface PostProps {
+  description?: boolean;
   post: Post & {
     tags: Tag[];
     authors: any;
   };
 }
 
-const Post: FC<PostProps> = ({ post }) => {
-  return (
-    <article className="w-full h-auto">
-      <div className="relative hover:-translate-y-2 transition-all duration-200">
-        <Link href={"/article/" + post.slug}>
-          <Image
-            className="object-cover rounded-2xl h-full max-h-[250px] aspect-square sm:aspect-auto sm:max-h-[679px]"
-            src={post.feature_image}
-            alt={post.title}
-            width={1920}
-            height={679}
-            loading="lazy"
-            placeholder="blur"
-            blurDataURL={`/_next/image?url=${post.feature_image}&w=1920&q=1`}
-          />
-        </Link>
-      </div>
-      <div>
-        <Tags tags={post.tags} />
-      </div>
-      <h1
-        className={cn(
-          "max-w-[95%]line-clamp-3 mt-5 text-[2rem] md:text-[4rem] font-bold hover:underline underline-offset-[3px] decoration-2",
-          FrauncesFont.className
-        )}
-        style={{ lineHeight: "1.2" }}
-      >
-        <Link href={"/article/" + post.slug}>{post.title}</Link>
-      </h1>
-      <p className="text-gray-700 sm:text-2xl line-clamp-6 mt-5 max-w-[80%]">
-        {post.custom_excerpt ? post.custom_excerpt : post.excerpt}
-      </p>
-      <div className="mt-10">
-        <div className="flex items-center gap-3">
-          <Image
-            src={post.authors[0].profile_image || ""}
-            width={45}
-            height={45}
-            className="bg-gray-400 object-cover min-w-[45px] min-h-[45px] rounded-full border"
-            alt={post.authors[0].slug}
-          />
-          <div className="flex flex-col">
-            by {post.authors[0].name}
-            <span className="text-xs">
-              {formatTimeToNow(new Date(post.created_at))} ∙ {post.reading_time}{" "}
-              mins read
-            </span>
+const Post = {
+  Big: async ({ post }: PostProps) => {
+    const blurImage = await getBase64(post.feature_image);
+
+    return (
+      <article className="w-full h-auto">
+        <div className="relative hover:-translate-y-2 transition-all duration-200">
+          <Link href={"/article/" + post.slug}>
+            <Image
+              className="object-cover rounded-2xl h-full max-h-[250px] aspect-square sm:aspect-auto sm:max-h-[679px]"
+              src={post.feature_image}
+              alt={post.title}
+              width={1920}
+              height={679}
+              priority
+              placeholder="blur"
+              blurDataURL={blurImage}
+            />
+          </Link>
+        </div>
+        <div>
+          <Tags tags={post.tags} />
+        </div>
+        <h1
+          className={cn(
+            "max-w-[95%] line-clamp-3 mt-5 text-[2rem] md:text-[4rem] font-bold hover:underline underline-offset-[3px] decoration-2",
+            FrauncesFont.className
+          )}
+          style={{ lineHeight: "1.2" }}
+        >
+          <Link href={"/article/" + post.slug}>{post.title}</Link>
+        </h1>
+        <p className="text-muted-foreground sm:text-2xl line-clamp-6 mt-5 max-w-[80%]">
+          {post.custom_excerpt ? post.custom_excerpt : post.excerpt}
+        </p>
+        <div className="mt-10">
+          <div className="flex items-center gap-3">
+            <Image
+              src={post.authors[0].profile_image || ""}
+              width={45}
+              height={45}
+              className="bg-gray-400 object-cover min-w-[45px] min-h-[45px] rounded-full border"
+              alt={post.authors[0].slug}
+            />
+            <div className="flex flex-col">
+              <span className="text-xs font-semibold">
+                by {post.authors[0].name}
+              </span>
+              <span className="text-xs">
+                {formatTimeToNow(new Date(post.created_at))} ∙{" "}
+                {post.reading_time} mins read
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    </article>
-  );
+      </article>
+    );
+  },
+  Default: async ({ post, description }: PostProps) => {
+    const blurImage = await getBase64(post.feature_image);
+    return (
+      <article className="w-full h-auto">
+        <div className="relative hover:-translate-y-2 transition-all duration-200">
+          <Link href={"/article/" + post.slug}>
+            <Image
+              className="object-cover rounded-2xl h-full max-h-[250px] aspect-square sm:aspect-auto sm:max-h-[300px]"
+              src={post.feature_image}
+              alt={post.title}
+              width={1920}
+              height={300}
+              priority
+              placeholder="blur"
+              blurDataURL={blurImage}
+            />
+          </Link>
+        </div>
+        <div>
+          <Tags tags={post.tags} />
+        </div>
+        <h1
+          className={cn(
+            "max-w-[95%] line-clamp-2 mt-5 text-[1rem] md:text-[2rem] font-bold hover:underline underline-offset-[3px] decoration-2",
+            FrauncesFont.className
+          )}
+          style={{ lineHeight: "1.2" }}
+        >
+          <Link href={"/article/" + post.slug}>{post.title}</Link>
+        </h1>
+        {description && (
+          <p className="text-muted-foreground sm:text-xl line-clamp-3 mt-5 max-w-[80%]">
+            {post.custom_excerpt ? post.custom_excerpt : post.excerpt}
+          </p>
+        )}
+        <div className="mt-3">
+          <div className="flex items-center gap-2 text-muted-foreground">
+            <Image
+              src={post.authors[0].profile_image || ""}
+              width={25}
+              height={25}
+              className="bg-gray-400 object-cover min-w-[25px] min-h-[25px] rounded-full border"
+              alt={post.authors[0].slug}
+            />
+            <div className="flex flex-col">
+              <span className="text-xs">by {post.authors[0].name}</span>
+            </div>
+          </div>
+        </div>
+      </article>
+    );
+  },
 };
 
 export default Post;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { ParamsTypes } from "../types";
-import { formatDistanceToNowStrict } from 'date-fns'
-import locale from 'date-fns/locale/en-US'
+import { formatDistanceToNowStrict } from "date-fns";
+import locale from "date-fns/locale/en-US";
+import { getPlaiceholder } from "plaiceholder";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -18,43 +19,42 @@ export async function api(url: string, params: ParamsTypes) {
   });
 }
 
-
 const formatDistanceLocale = {
-  lessThanXSeconds: 'just now',
-  xSeconds: 'just now',
-  halfAMinute: 'just now',
-  lessThanXMinutes: '{{count}}m',
-  xMinutes: '{{count}}m',
-  aboutXHours: '{{count}}h',
-  xHours: '{{count}}h',
-  xDays: '{{count}}d',
-  aboutXWeeks: '{{count}}w',
-  xWeeks: '{{count}}w',
-  aboutXMonths: '{{count}}m',
-  xMonths: '{{count}}m',
-  aboutXYears: '{{count}}y',
-  xYears: '{{count}}y',
-  overXYears: '{{count}}y',
-  almostXYears: '{{count}}y',
-}
+  lessThanXSeconds: "just now",
+  xSeconds: "just now",
+  halfAMinute: "just now",
+  lessThanXMinutes: "{{count}}m",
+  xMinutes: "{{count}}m",
+  aboutXHours: "{{count}}h",
+  xHours: "{{count}}h",
+  xDays: "{{count}}d",
+  aboutXWeeks: "{{count}}w",
+  xWeeks: "{{count}}w",
+  aboutXMonths: "{{count}}m",
+  xMonths: "{{count}}m",
+  aboutXYears: "{{count}}y",
+  xYears: "{{count}}y",
+  overXYears: "{{count}}y",
+  almostXYears: "{{count}}y",
+};
 
 function formatDistance(token: string, count: number, options?: any): string {
-  options = options || {}
+  options = options || {};
 
   const result = formatDistanceLocale[
     token as keyof typeof formatDistanceLocale
-  ].replace('{{count}}', count.toString())
+  ].replace("{{count}}", count.toString());
 
   if (options.addSuffix) {
     if (options.comparison > 0) {
-      return 'in ' + result
+      return "in " + result;
     } else {
-      if (result === 'just now') return result
-      return result + ' ago'
+      if (result === "just now") return result;
+      return result + " ago";
     }
   }
 
-  return result
+  return result;
 }
 
 export function formatTimeToNow(date: Date): string {
@@ -64,5 +64,25 @@ export function formatTimeToNow(date: Date): string {
       ...locale,
       formatDistance,
     },
-  })
+  });
+}
+
+export default async function getBase64(imageUrl: string) {
+  try {
+    const res = await fetch(imageUrl);
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch image: ${res.status} ${res.statusText}`);
+    }
+
+    const buffer = await res.arrayBuffer();
+
+    const { base64 } = await getPlaiceholder(Buffer.from(buffer));
+
+    //console.log(`base64: ${base64}`)
+
+    return base64;
+  } catch (e) {
+    if (e instanceof Error) console.log(e.stack);
+  }
 }
