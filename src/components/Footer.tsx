22cfe464b5d2543f2a8f9aa9a7b5c05b072408bf
dv0C <tasosmeidanis12@gmail.com>
2024-01-03 12:20:45 +0200

    modified:   src/components/Footer.tsx 	new file:   src/configs/FooterList.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookIcon, Twitter, Youtube } from "lucide-react";
+import { FooterList } from "@/configs/FooterList";
 import Link from "next/link";
 import React from "react";
 
@@ -9,108 +9,7 @@ interface ItemProps {
 }
 
 const Footer = () => {
-  const list = [
-    {
-      heading: "Social",
-      items: [
-        {
-          name: "Facebook",
-          icon: <FacebookIcon size={18} />,
-          href: "/facebook",
-        },
-        {
-          name: "Twitter",
-          icon: <Twitter size={18} />,
-          href: "/twitter",
-        },
-        {
-          name: "Youtube",
-          icon: <Youtube size={18} />,
-          href: "/youtube",
-        },
-      ],
-    },
-    {
-      heading: "Social",
-      items: [
-        {
-          name: "Facebook",
-          icon: <FacebookIcon size={18} />,
-          href: "/facebook",
-        },
-        {
-          name: "Twitter",
-          icon: <Twitter size={18} />,
-          href: "/twitter",
-        },
-        {
-          name: "Youtube",
-          icon: <Youtube size={18} />,
-          href: "/youtube",
-        },
-      ],
-    },
-    {
-      heading: "Social",
-      items: [
-        {
-          name: "Facebook",
-          icon: <FacebookIcon size={18} />,
-          href: "/facebook",
-        },
-        {
-          name: "Twitter",
-          icon: <Twitter size={18} />,
-          href: "/twitter",
-        },
-        {
-          name: "Youtube",
-          icon: <Youtube size={18} />,
-          href: "/youtube",
-        },
-      ],
-    },
-    {
-      heading: "Social",
-      items: [
-        {
-          name: "Facebook",
-          icon: <FacebookIcon size={18} />,
-          href: "/facebook",
-        },
-        {
-          name: "Twitter",
-          icon: <Twitter size={18} />,
-          href: "/twitter",
-        },
-        {
-          name: "Youtube",
-          icon: <Youtube size={18} />,
-          href: "/youtube",
-        },
-      ],
-    },
-    {
-      heading: "Social",
-      items: [
-        {
-          name: "Facebook",
-          icon: <FacebookIcon size={18} />,
-          href: "/facebook",
-        },
-        {
-          name: "Twitter",
-          icon: <Twitter size={18} />,
-          href: "/twitter",
-        },
-        {
-          name: "Youtube",
-          icon: <Youtube size={18} />,
-          href: "/youtube",
-        },
-      ],
-    },
-  ];
+  const list = FooterList;
   return (
     <div className="mt-20 mx-auto max-w-[var(--max-width-2)] py-10 p-5 md:p-0 md:py-10 border-t">
       <div className="flex flex-wrap gap-10">
@@ -132,7 +31,7 @@ const List = ({ list }: any) => {
     <div className="flex flex-wrap justify-between items-center m-auto w-full gap-10">
       {list.map((item: any, i: number) => (
         <div className="flex flex-col" key={i}>
-          <h1 className="font-semibold mb-3">{item.heading}</h1>
+          <h1 className="font-semibold mb-3 text-sm">{item.heading}</h1>
           <ul className="space-y-2">
             {item.items.map((item: ItemProps, i: number) => (
               <li key={i}>
