5fbded88a830d051aa0eae9724925ec7f626c048
dv0c <tasosmeidanis12@gmail.com>
2024-01-16 22:19:38 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/SearchBar.tsx 	new file:   src/components/SearchPopup.tsx 	modified:   src/components/Tags.tsx 	new file:   src/components/ui/command.tsx

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -14,17 +14,17 @@ const Tags = ({
   return (
     <div className={cn("flex items-center mt-5 flex-wrap", gap)}>
       {tags.map((item, i) => (
-        <button
-          className={cn(
-            "bg-gray-100 transition-all hover:bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold",
-            className
-          )}
-          key={i}
-        >
-          <Link href={"/tag/" + item.name}>
+        <Link href={"/tag/" + item.name}>
+          <button
+            className={cn(
+              "bg-gray-100 transition-all hover:bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold",
+              className
+            )}
+            key={i}
+          >
             <span className="truncate">{item.name}</span>
-          </Link>
-        </button>
+          </button>
+        </Link>
       ))}
     </div>
   );
